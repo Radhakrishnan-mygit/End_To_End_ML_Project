c2d01e70610cb7cf35c4ef9a6cf609ff097dacdc
Radhakrishnan R <knownradha7@gmail.com>
2024-12-09 19:39:03 +0530

    eda perfomed

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_details:sys):
    _,_,exc=error_details.exc_info()
@@ -19,9 +19,10 @@ class CustomException(Exception):
       return self.error_message
 
 
-# if __name__=="__main__":
-#    try:
-#       a=1/0
-#    except Exception as e:
-#       logging.INFO("Divide by zero error")
-#       raise CustomException(e,sys)
+if __name__=="__main__":
+   try:
+      a=1/0
+   except Exception as e:
+      logging.INFO("Divide by zero error ")
+      raise CustomException(e,sys)
+
